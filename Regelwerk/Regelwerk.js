8c38e77658f563d21238169a543f4436f4f8fd11
Jens Bannmann <jens.b@web.de>
2014-08-06 22:10:18 +0200

    Inhaltsverzeichnis hinzugefügt
    - Issue #3

--- Regelwerk/Regelwerk.js
+++ Regelwerk/Regelwerk.js
@@ -1,24 +1,51 @@
 $(function() {
-  var container = $("nav");
-  var lastLevel = 0;
-  $("main").add("#appendices").find("section").each(function(index, htmlElement) {
-                                                    var element = $(htmlElement);
-                                                    var text = element.find("> h1").text();
-                                                    console.log(text);
-                                                    var level = element.parents("section").length + 1;
-                                                    console.log("level = "+level);
-                                                    if (level > lastLevel) {
-                                                        container = $("<ol>").appendTo(container);
-                                                    } else {
-                                                        for (var i = lastLevel; i > level; i--) {
-                                                            container = container.parent();
-                                                        }
-                                                    }
-                                                    $("<li>").text(text).appendTo(container);
-                                                    lastLevel = level;
-                                                    
-                                                    // Spätestens fürs Stichwortverzeichnis funktioniert das nicht, denn es hat keine Nummer.
+    var maxLevel = 2;
+    function generateToc(rootSection, targetElement, rootSectionTemplate,
+            subSectionTemplate) {
+        if (!subSectionTemplate) {
+            subSectionTemplate = rootSectionTemplate;
+        }
+        var lastLevel = 0;
+        var container = targetElement;
+        rootSection.find("section").each(
+                function(index, htmlElement) {
+                    var element = $(htmlElement);
+                    var text = element.find("> h1").text();
+                    console.log(text);
+                    var level = element.parents("section").length + 1;
 
-                                                    
-                         });
+                    if (level <= maxLevel) {
+                        console.log("level = " + level);
+                        if (level > lastLevel) {
+                            if (level == 1) {
+                                container = $(rootSectionTemplate).appendTo(
+                                        container);
+                            } else {
+                                container = $(subSectionTemplate).appendTo(
+                                        container);
+                            }
+                        } else {
+                            for (var i = lastLevel; i > level; i--) {
+                                container = container.parent();
+                            }
+                        }
+
+                        if (!element.attr("id")) {
+                            element.attr("id", new String(Math.random())
+                                    .substr(2));
+                        }
+
+                        var link = $("<a>").attr("href",
+                                "#" + element.attr("id")).text(text);
+
+                        $("<li>").append(link).appendTo(container);
+                        lastLevel = level;
+                    }
+                });
+    }
+
+    var container = $("nav");
+    generateToc($("main"), container, "<ol>");
+    generateToc($("#appendices"), container, "<ol class='A'>", "<ul>");
+    generateToc($("#backmatter"), container, "<ul>");
 });
