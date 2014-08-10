133ac5eafaea4dd210d474bd4e829f0af5d9e9aa
Jens Bannmann <jens.b@web.de>
2014-08-10 19:01:10 +0200

    Automatische Verweistitel
    - Issue #14
    - Verweise entsprechend überarbeitet
    - Außerdem: allgemeine Fehlerbehandlung via logError()

--- Regelwerk/Regelwerk.js
+++ Regelwerk/Regelwerk.js
@@ -1,4 +1,42 @@
 $(function() {
+    function logError() {
+        $("body").addClass("error");
+        console.error.apply(console, arguments);
+    }
+
+    function processLink(index, domElement) {
+        var element = $(domElement);
+        var href = element.attr("href");
+        var targetElement = $(href);
+        if (targetElement.length != 1) {
+            logError("invalid ID ref: ", element.get(0));
+        } else {
+            if (element.is(":empty")) {
+                var headingText;
+                if (targetElement.is("dt")) {
+                    headingText = targetElement.text();
+                } else {
+                    var heading = targetElement.find("h1:first");
+                    if (heading.length == 1) {
+                        headingText = heading.text();
+                    }
+                }
+
+                if (headingText) {
+                    element.text(headingText);
+                } else {
+                    logError("could not identify heading for", targetElement
+                            .get(0), "linked from", element.get(0));
+                }
+            } else {
+                var label = element.text().replace(/\s+/g, " ");
+                console.debug("custom label '" + label + "' at",
+                        element.get(0), "linking to", targetElement.get(0));
+            }
+        }
+    }
+    $("a[href^=#]").each(processLink);
+
     var maxLevel = 2;
     function generateToc(rootSection, targetElement, rootSectionTemplate,
             subSectionTemplate) {
@@ -11,11 +49,9 @@ $(function() {
                 function(index, htmlElement) {
                     var element = $(htmlElement);
                     var text = element.find("> h1").text();
-                    console.log(text);
                     var level = element.parents("section").length + 1;
 
                     if (level <= maxLevel) {
-                        console.log("level = " + level);
                         if (level > lastLevel) {
                             if (level == 1) {
                                 container = $(rootSectionTemplate).appendTo(
